339191ce587abfbc29ac63ed1fa815d8b17315ca
Pavel <pmokhliakov@gmail.com>
2020-10-11 18:19:42 +0300

    OOP

--- 2_course/OOP/LB2/ip.cpp
+++ 2_course/OOP/LB2/ip.cpp
@@ -1,1 +1,44 @@
 #include "ip.hpp"
+
+//Конструкторы
+
+NIP::IPAddress::IPAddress()
+{
+    for(int i = 0;i < 4;++i)
+    {
+        bites[i]=0;
+    }
+}
+
+NIP::IPAddress::IPAddress(unsigned char b1,unsigned char b2,unsigned char b3,unsigned char b4) 
+{
+    bites[0] = b1;
+    bites[1] = b2;
+    bites[2] = b3;
+    bites[3] = b4;
+}
+
+NIP::IPAddress::IPAddress(const NIP::IPAddress::IPAddress& b)
+{
+    bites[0] = b.bites[0];
+    bites[1] = b.bites[1];
+    bites[2] = b.bites[2];
+    bites[3] = b.bites[3];
+}
+
+
+//Оператор присваивания
+void NIP::IPAddress::operator =(const NIP::IPAddress::IPAddress& b)
+{
+    bites[0] = b.bites[0];
+    bites[1] = b.bites[1];
+    bites[2] = b.bites[2];
+    bites[3] = b.bites[3];
+}
+
+//Оператор возврата битов
+unsigned char& NIP::IPAddress::operator [](int i)
+{
+    return bites[i];
+}
+
--- 2_course/OOP/LB2/ip.hpp
+++ 2_course/OOP/LB2/ip.hpp
@@ -1,13 +1,35 @@
 
 namespace NIP
 {
-    class TIP
+    class IPAddress
     {
         protected:
         unsigned char bites[4];
-        
+
+        //Конструкторы
+        IPAddress();
+        IPAddress(unsigned char b1,unsigned char b2,unsigned char b3,unsigned char b4);
+        IPAddress(const IPAddress& b);
+
+        //Оператор присваивания
+        void operator =(const IPAddress& b);
+        //Оператор возврата битов
+        unsigned char& operator [](int i); 
+        //Оператовы арифметических действий 
+        IPAddress operator +(const IPAddress& b);
+        IPAddress operator -(const IPAddress& b);
+        //Операторы сравнения 
+        bool operator ==(const IPAddress& b);
+        bool operator >(const IPAddress& b);
+        bool operator <(const IPAddress& b);
+        bool operator >=(const IPAddress& b);
+        bool operator <=(const IPAddress& b);
 
 
+        //Коньюнкция по маске
+        void from_mask(const IPAddress& mask);
+
+        bool ip_in_net(const IPAddress& ip_net,const IPAddress& mask);
 
     }; //class TIP
 } //namespace NIP
