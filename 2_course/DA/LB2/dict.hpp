5b4537ce185251f665bd8f75167bffee890f69c5
Pavel <pmokhliakov@gmail.com>
2020-11-19 16:43:36 +0300

    DA

--- 2_course/DA/LB2/dict.hpp
+++ 2_course/DA/LB2/dict.hpp
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "pair.hpp"
+#include "string.hpp"
+#include "rb_vect.hpp"
+
+#include <iostream>
+#include <cstring>
+
+void parse_comand(NString::TString str)
+{
+    if(str[0] == '+')
+    {
+        int word_end = str.find(' ',2);
+        NString::TString word;
+        NString::TString num;
+        std::memcpy(word.str,&str.str[2],word_end-2);
+        word.size = word_end-2;
+        std::memcpy(num.str,&str.str[word_end+1],str.size - (word_end+2));
+        num.size = str.size - (word_end+2);
+
+
+    }
+}
--- 2_course/DA/LB2/string.hpp
+++ 2_course/DA/LB2/string.hpp
@@ -6,12 +6,16 @@ namespace NString
 {
     class TString
     {
-        private:
-            char* str;
+            
         public:
+            char* str;
+            size_t size;
+
+            
             TString()
             {
                 str = new char[256];
+                size = 0;
             }
 
             void operator=(const TString &second)
@@ -61,49 +65,52 @@ namespace NString
                 return false;
             }
 
-         friend std::ostream& operator<< (std::ostream &out, const TString &str)
-        {
-            out << str.str;
-            return out;
-        }
-
-        friend std::istream& operator>>(std::istream &in, TString &str)
-        {
-            
-            in >> str.str;
-            return in;
-        }
-
-        int find(char ch)
-        {
-            for(int i=0;i<256;++i)
+            friend std::ostream& operator<< (std::ostream &out, const TString &str)
             {
-                if(this->str[i] == ch)
-                    return i;
-                if(this->str[i] == '\0')
-                    return -1;
+                out << str.str;
+                return out;
             }
-            return -1;
-        }
 
-        int find(const char &ch, int &start)
-        {
-            if(start > 256)
-                start = 256;
-            for(int i=start;i<256;++i)
+            friend std::istream& operator>>(std::istream &in, TString &str)
             {
-                if(this->str[i] == ch)
-                    return i;
-                if(this->str[i] == '\0')
-                    return -1;
-            }
-            return -1;
-        }
+                
+                in >> str.str;
 
-        char &operator[](size_t index)
-        {
-            return this->str[index];
-        }
+                while(str.str[str.size] != '\0')
+                    ++str.size;
+                return in;
+            }
+
+            int find(char ch)
+            {
+                for(int i=0;i<256;++i)
+                {
+                    if(this->str[i] == ch)
+                        return i;
+                    if(this->str[i] == '\0')
+                        return -1;
+                }
+                return -1;
+            }
+
+            int find(const char ch, int start)
+            {
+                if(start > 256)
+                    start = 256;
+                for(int i=start;i<256;++i)
+                {
+                    if(this->str[i] == ch)
+                        return i;
+                    if(this->str[i] == '\0')
+                        return -1;
+                }
+                return -1;
+            }
+
+            char &operator[](size_t index)
+            {
+                return this->str[index];
+            }
         
     };
 } //namespace NString
