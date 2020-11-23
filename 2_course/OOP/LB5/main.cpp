850d7dab3ff2984ceab1efc60e6d0c4a2c034469
gametwix <pmokhliakov@gmail.com>
2020-11-23 18:58:11 +0300

    OOP

--- 2_course/OOP/LB5/main.cpp
+++ 2_course/OOP/LB5/main.cpp
@@ -1,19 +1,118 @@
 #include <iostream>
+#include <algorithm>
 #include "queue.hpp"
+#include "squere.hpp"
 
 int main()
 {
-    Queue<int> q;
-
+    Queue<Square<int>> que;
+/*
     q.Push(1);
     q.Push(2);
     q.Push(3);
 
-    q.Pop();
-    q.Pop();
-    q.Pop();
+    q.Insert((q.Begin()+1),5);
+    q.Erase(q.Begin()+2);
 
-    Queue<int>::iterator a;
+
+
+    //Queue<int>::iterator a = q.Begin();
+
+
+
+
+    auto print = [](int n) { std::cout << " " << n; };
+    std::for_each(q.Begin(),q.End(),print);
+
+    ///std::cout << 1;
+    for (Queue<int>::iterator iter = q.Begin(); iter != q.End(); ++iter)
+    {
+        std::cout <<*iter << std::endl;
+    }*/
+
+
+     while(true)
+    {
+        int sw=-1;
+        Square<int> sq;
+        int pos = 0;
+        
+        int minimal_suare = 0;
+        auto print = [](Square<int> n) { std::cout << n << std::endl; };
+        
+
+        std::cout << "0 - Выход" << std::endl;
+        std::cout << "1 - Добавить квадрат в конец очереди" << std::endl;
+        std::cout << "2 - Добавить квадрат по индексу" << std::endl;
+        std::cout << "3 - Удалить квадрат в начале очереди" << std::endl;
+        std::cout << "4 - Удалить квадрат по индексу" << std::endl;
+        std::cout << "5 - Вывести очередь" << std::endl;
+        std::cout << "6 - Вывести количество квадратов площадь которых меньше указанной" << std::endl;
+        std::cout << "Выберите действие: ";
+        std::cin >> sw;
+
+        if(sw == 0) break;
+        else
+        {
+            switch (sw)
+            {
+            case 1:
+                std::cout << "Введите длину стороны квадрата:";
+                std::cin >> sq;
+                que.Push(sq);
+                std::cout << "Добавление произведено успешно" << std::endl;
+                break;
+            case 2:
+                std::cout << "Введите длину стороны квадрата:" ;
+                std::cin >> sq;
+                std::cout << "Введите индекс:" ;
+                std::cin >> pos;
+                try
+                {
+                    que.Insert(que.Begin()+(pos-1),sq);
+                    std::cout << "Добавление произведено успешно" << std::endl;
+                }
+                catch(int exs)
+                {
+                    std::cout << "Выход за допустимую границу коллекции" << std::endl;
+                }
+                break;
+            case 3:
+                que.Pop();
+                std::cout << "Удаление произведено успешно" << std::endl;
+
+                break;
+            case 4:
+                std::cout << "Введите индекс:";
+                std::cin >> pos;
+                try
+                {
+                    que.Erase(que.Begin()+(pos-1));
+                    std::cout << "Удаление произведено успешно" << std::endl;
+                }
+                catch(int exs)
+                {
+                    std::cout << "Выход за допустимую границу коллекции" << std::endl;
+                }
+            case 5:
+                
+                std::for_each(que.Begin(),que.End(),print);
+                break;
+            case 6:
+            
+                
+                std::cout << "Введите минимальную площадь:" ;
+                std::cin >> minimal_suare;
+                auto figures_less_squre = [minimal_suare](Square<int> n) { return (n.square() < minimal_suare); };
+                std::cout << "Количество квадратов площадь которых меньше указанной: " << std::count_if(que.Begin(), que.End(), figures_less_squre) << std::endl;
+
+                
+            
+            }
+        }
+    }
+
+
 
     return 0;
 }
--- 2_course/OOP/LB5/queue.hpp
+++ 2_course/OOP/LB5/queue.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <memory>
-
+#include <iostream>
 
 template <typename T>
 class Queue
@@ -20,7 +20,7 @@ class Queue
                     next = nullptr;
                     prev = nullptr;
                 }
-                Queue_elem(const int data)
+                Queue_elem(const Q data)
                 {
                     key = data;
                     next = nullptr;
@@ -31,11 +31,12 @@ class Queue
     size_t size = 0;
     std::shared_ptr<Queue_elem<T>> start;
     std::shared_ptr<Queue_elem<T>> finish;
+    std::shared_ptr<Queue_elem<T>> Nil;
 
 
     Queue_elem<T>* Top()
     {
-        return finish;
+        return start;
     }
 
     public:
@@ -44,29 +45,38 @@ class Queue
         {
             start = nullptr;
             finish = nullptr;
+            Nil = std::shared_ptr<Queue_elem<T>>(new Queue_elem<T>);
+            Nil->prev = nullptr;
+            Nil->next = start;
         }
 
 
         void Push(const T& data)
         {
-            std::shared_ptr<Queue_elem<T>> elem(new Queue_elem<T>(data));
+            Queue_elem<T>* l = new Queue_elem<T>(data);
+            std::shared_ptr<Queue_elem<T>> elem(l);
             if(start == nullptr)
             {
-                start = elem;
                 finish = elem;
             }
             else
             {
                 elem->next = start;
                 start->prev = elem;
-                start = elem;
             }
+            elem->prev = Nil;
+            Nil->next = elem;
+            start = elem;
             ++size;
         }
 
 
         T Pop()
         {
+            if(finish == nullptr)
+            {
+                throw -1;
+            }
             T data = finish->key;
             if(finish->prev == nullptr)
             {
@@ -76,7 +86,6 @@ class Queue
             else
             {
                 finish = finish->prev;
-                finish->next = nullptr;
             }
             --size;
             return data;
@@ -84,30 +93,146 @@ class Queue
 
         class iterator
         {
-            T* data_iter;
+            
+            Queue_elem<T>* data_iter;
             public:
-                T* operator->()
+            using value_type = T;
+            using reference = T&;
+            using pointer = T*;
+            using difference_type = ptrdiff_t;
+            using iterator_category = std::forward_iterator_tag;
+
+                iterator()
                 {
-                    return data_iter;
+                    data_iter = nullptr;
+                }
+                
+                iterator(Queue_elem<T>* second)
+                {
+                    data_iter = second;
                 }
 
-                T* operator*()
+                T*& operator->()
                 {
-                    return *data_iter;
+                    return &data_iter->key;
                 }
 
-                bool operator==(const iterator& second)
+                T& operator*()
                 {
-                    return data_iter == second.data_iter;
+                    return data_iter->key;
                 }
 
-                bool operator==(const T& second)
+                bool operator==(const iterator &second)
                 {
-                    return data_iter == second;
+                    return (data_iter == second.data_iter);
+                }
+
+                bool operator==(const T* &second)
+                {
+                    return (data_iter == second);
+                }
+
+                bool operator!=(const iterator &second)
+                {
+                    return (data_iter != second.data_iter);
+                }
+
+                bool operator!=(const T* &second)
+                {
+                    return (data_iter != second);
+                }
+
+                void operator=(const iterator &second)
+                {
+                    data_iter = second.data_iter;
+                }
+
+                void operator=(const T* &second)
+                {
+                    data_iter = second;
+                }
+
+                iterator operator++()
+                {
+                    data_iter = data_iter->prev.get();
+                    return iterator(data_iter);
+                }
+
+                iterator operator--()
+                {
+                    data_iter = data_iter->next.get();
+                    return iterator(data_iter);
+                }
+
+                iterator operator+(const size_t &ind)
+                {
+                    iterator it(data_iter);
+                    for(int i=0;i<ind;++i)
+                    {
+                        it.data_iter = it.data_iter->prev.get();
+                    }
+                    return it;
+                }
+
+                iterator operator-(const size_t &ind)
+                {
+                    iterator it(data_iter);
+                    for(int i=0;i<ind;++i)
+                    {
+                        it.data_iter = it.data_iter->next.get();
+                    }
+                    return it;
                 }
         };
 
+        iterator Begin()
+        {
+            return iterator(finish.get());
+            
+        }
 
+        iterator End()
+        {
+            iterator iter = start.get();
+            ++iter;
+            return (iter);
+        }
 
+        void Insert(iterator it,T data)
+        {
+            if(it == nullptr)
+            {
+                throw -1;
+            }
+            if(it == End())
+                Push(data);
+            else
+            {
+                Push(T());
+                for(iterator iter = End() - 1;iter != it;--iter)
+                {
+                    *iter = *(iter-1);
+                }
+                *it = data;
+            }
+            
+        }
 
+        void Erase(iterator it)
+        {
+            if((it == nullptr) || (it == Nil.get()))
+            {
+                throw -1;
+            }
+            for(iterator iter = it;iter != Begin();--iter)
+            {
+                *iter = *(iter-1);
+            }
+            Pop();
+        }
+
+        int Size()
+        {
+            return size;
+        }
 };
