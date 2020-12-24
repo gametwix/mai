a0f0ee4c1b3646b7c49135e7f5a69c67d172259a
gametwix <pmokhliakov@gmail.com>
2020-12-24 22:29:17 +0300

    DA

--- 2_course/DA/LB4/ahok.hpp
+++ 2_course/DA/LB4/ahok.hpp
@@ -2,26 +2,58 @@
 #include <memory>
 #include <unordered_map>
 #include <vector>
+#include <stdexcept>
+#include <queue>
 
 
 class TAhoKarasik{
 	private:
 		class TElem{
+			using elem_ptr = TElem*;
 			public:
-				using elem_ptr = std::shared_ptr<TElem>;
 				long long Sym;
 				bool IsLast;
 				elem_ptr Suf;
 				elem_ptr Stop;
+				elem_ptr Parent;
 				std::unordered_map<long long,elem_ptr> Childs;
 			public:
-				TElem(long long S = -1,bool IsL=false):Sym(S),IsLast(IsL),Suf(nullptr),Stop(nullptr){}
+				TElem(long long S = -1,bool IsL=false):Sym(S),IsLast(IsL),Suf(nullptr),Stop(nullptr),Parent(nullptr){}
+				~TElem(){
+					for(auto& i: Childs)
+					{
+						delete(i.second);
+					}
+				};
 		}; //TElem
+		using elem_ptr = TElem*;
 	public:
 		TElem Root;
-		void Push(std::vector<long long>& vect)
-		{
-			
+		~TAhoKarasik(){};
+		void Push(std::vector<long long>& vect){
+			elem_ptr cur_elem(&Root);
+			size_t size_vect = vect.size();
+			elem_ptr child;
+			for(int i = 0;i < size_vect;++i)
+			{
+				try
+				{	
+					std::cout << cur_elem->Childs.at(vect[i]) <<std::endl;
+					cur_elem = cur_elem->Childs.at(vect[i]);
+				}
+				catch(std::out_of_range)
+				{
+					child = new TElem(vect[i]);
+					cur_elem->Childs.insert({vect[i],child});
+					child->Parent = cur_elem;
+					cur_elem = child;
+				}
+			}
+			cur_elem->IsLast = true;
 		}
 
+		void SearchSufPtr()
+		{
+			std::queue<TElem*> q;
+		}
 }; //TAhoKarasik
--- 2_course/DA/LB4/main.cpp
+++ 2_course/DA/LB4/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
+#include "ahok.hpp"
 
 int main()
 {
-	
+    TAhoKarasik ahk;
+    std::vector<long long> a = {1,2,3,4,5};
+    std::vector<long long> b = {1,2,6,7};
+    ahk.Push(a);
+    ahk.Push(b);
+    return 0;	
 }
