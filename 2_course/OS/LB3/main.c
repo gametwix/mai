eba15999cf5a8cb9e8ccd419e8f5281869785782
gametwix <pmokhliakov@gmail.com>
2021-01-27 23:56:34 +0300

    OS LB3:one thread

--- 2_course/OS/LB3/main.c
+++ 2_course/OS/LB3/main.c
@@ -4,7 +4,12 @@
 #define UP 1
 #define DOWN 0
 
-int Ceil(int a, int b){ return (a+a%2)/b;}
+int SizeStep(int Num){
+    int i = 1;
+    while(i < Num)
+        i*=2;
+    return i;
+}
 
 void comparator(int *Nums,size_t i,size_t j,int dir){
     if(dir == (Nums[i] > Nums[j])){
@@ -14,29 +19,47 @@ void comparator(int *Nums,size_t i,size_t j,int dir){
     }
 }
 
-void BironicMerge(int *Nums,size_t Size,size_t Start,int Dir){
-
-}
-
-void BitonicSort(int *Nums,size_t Size,size_t Start,int Dir){
+void BitonicMerge(int *Nums,size_t Size,size_t RealSize,size_t Start,int Dir){
     if(Size > 1){
-        int k = Ceil(Size,2);
-        BitonicSort(Nums,k,0,UP);
-        BitonicSort(Nums,Size-k,k,UP);
-
+        int k = Size / 2;
+        int n = RealSize;
+        for(int i=Start;(i<k+Start && i < n);++i)
+        {
+            if(i+k < n)
+                comparator(Nums,i,i+k,Dir);
+        }
+        BitonicMerge(Nums,k,RealSize,0,Dir);
+        BitonicMerge(Nums,k,RealSize,k,Dir);
     }
 }
+ 
+void BitonicSort(int *Nums,size_t Size,size_t RealSize,size_t Start,int Dir){
+    if(Size > 1 && (Start+1 < RealSize)){
+        int k = Size/2;
+        BitonicSort(Nums,k,RealSize,Start,DOWN);
+        BitonicSort(Nums,k,RealSize,Start+k,UP);
+        BitonicMerge(Nums,Size,RealSize,Start,Dir);
+    }
+}
+
+
 
 
 int main(){
     int n;
     scanf("%d",&n);
-    /*int *Nums = malloc(sizeof(int)*n);
-
+    int *Nums = malloc(sizeof(int)*n);
+    int SizeToSort = SizeStep(n);
     for(int i = 0;i<n;++i)
         scanf("%d",Nums+i);
-    free(Nums);*/
-    printf("%d",Ceil(n,2));
-    
+
+    BitonicSort(Nums,SizeToSort,n,0,UP);
+
+    for(int i=0;i<n;++i){
+        printf("%d ",Nums[i]);
+    }
+    printf("\n");
+
+    free(Nums);
     return 0;
 }
