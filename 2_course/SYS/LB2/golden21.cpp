38d87f51877f75712143989fb5ef01f93055e2e3
gametwix <pmokhliakov@gmail.com>
2021-03-22 17:21:12 +0300

    SYS

--- 2_course/SYS/LB2/golden21.cpp
+++ 2_course/SYS/LB2/golden21.cpp
@@ -24,7 +24,7 @@ double fun(double x){
 
 double golden__section__search(double a,double b){
     {
-        double xmin = ((a < b) ? golden__start(a,b):golden__start(b,a));
+        double xmin((a < b) ? golden__start(a,b):golden__start(b,a));
         newline();
         return xmin;
     }
@@ -33,13 +33,13 @@ double golden__section__search(double a,double b){
 double golden__start(double a,double b){
     total__iterations = 0.;
     {
-        double xa = a + mphi*(b-a),
-            xb = b + (- mphi*(b - a));
+        double xa(a + mphi*(b-a)),
+               xb(b + (- mphi*(b - a)));
         return __MPA__try(a,b,xa,fun(xa),xb,fun(xb));
     }
 }
 
-double mphi = (3. - sqrt(5.))*(1.0 / 2.0);
+double mphi = ((3. - sqrt(5.))*(1. / 2.));
 
 double __MPA__try(double a,double b, 
                 double xa, double ya,
