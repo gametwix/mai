73d7abb0e9ec842506d26ad287a0a4e15b80cd4b
gametwix <pmokhliakov@gmail.com>
2021-04-11 17:29:57 +0300

    first_test

--- 2_course/OS/LB6_8/main.cpp
+++ 2_course/OS/LB6_8/main.cpp
@@ -1,43 +1,23 @@
-//
-//  Weather update client in C++
-//  Connects SUB socket to tcp://localhost:5556
-//  Collects weather updates and finds avg temp in zipcode
-//
+#include "node.hpp"
+#include "net_func.hpp"
 
-#include <zmq.hpp>
-#include <iostream>
-#include <sstream>
 
-int main (int argc, char *argv[])
-{
-    zmq::context_t context (1);
 
-    //  Socket to talk to server
-    std::cout << "Collecting updates from weather server...\n" << std::endl;
-    zmq::socket_t subscriber (context, ZMQ_SUB);
-    subscriber.connect("tcp://localhost:5556");
+int main(){
 
-    //  Subscribe to zipcode, default is NYC, 10001
-	const char *filter = (argc > 1)? argv [1]: "10001 ";
-    subscriber.setsockopt(ZMQ_SUBSCRIBE, filter, strlen (filter));
+    Node me(-1);
+    
+    while(1){
+        std::string message;
+        std::string command;
+        std::cin >> command;
 
-    //  Process 100 updates
-    int update_nbr;
-    long total_temp = 0;
-    for (update_nbr = 0; update_nbr < 100; update_nbr++) {
-
-        zmq::message_t update;
-        int zipcode, temperature, relhumidity;
-
-        subscriber.recv(&update);
-
-        std::istringstream iss(static_cast<char*>(update.data()));
-		iss >> zipcode >> temperature >> relhumidity ;
-
-		total_temp += temperature;
+        if(command == "create"){
+            int id_child, id_parent;
+            std::string prog_path = "./work";
+            std::cin >> id_child >> id_parent;
+            me.Create(id_parent, id_child, prog_path);
+        }
     }
-    std::cout 	<< "Average temperature for zipcode '"<< filter
-    			<<"' was "<<(int) (total_temp / update_nbr) <<"F"
-    			<< std::endl;
     return 0;
 }
