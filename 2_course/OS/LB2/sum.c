80be7323d647fa33da018450ca953353239dcfdc
gametwix <pmokhliakov@gmail.com>
2021-04-14 18:26:02 +0300

    fix child reading filename

--- 2_course/OS/LB2/sum.c
+++ 2_course/OS/LB2/sum.c
@@ -1,12 +1,15 @@
 #include "stdio.h"
+#include "string.h"
 
 int main(){
     long long sum = 0;
     int num;
     char ch;
     FILE *file;
-    char filename[100];
-    scanf("%s",filename);
+    char *filename = NULL;
+    size_t sizename = 0;
+    getline(&filename,&sizename,stdin);
+    filename[strlen(filename)-1] = '\0';
     file = fopen(filename, "w");
     while(scanf("%d%c",&num,&ch) != EOF){
         sum += num;
